16864361564814196e5e337e58927c35ccf9e21c
geremewatalay <geremewatalay@gmail.com>
2024-12-03 12:52:27 -0800

    crawling pages commit

--- spider.py
+++ spider.py
@@ -22,4 +22,22 @@ class Spider:
         Spider.crawled_file = Spider.project_name + '/crawled.txt'
         self.boot()
         self.crawl_page('First spider', Spider.base_url)
+    @staticmethod
+    def boot(self):
+        create_project_dir(Spider.project_name)
+        create_data_files(Spider.project_name, Spider.base_url)
+        Spider.queue = file_to_set(Spider.queue_file)
+        Spider.crawled = file_to_set(Spider.crawled_file)
+    
+
+    @staticmethod
+    def crawl_page(thread_name, page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name + 'now crawing' + page_url)
+            print('Queue' + str(len(Spider.queue)) + ' | Crawled' +str(len(Spider.crawled)))
+            Spider.add_links_to_queue(Spider.gather_link(page_url))
+            Spider.queue.remove(page_url)
+            Spider.crawled.add(page_url)
+            Spider.update_files()
+
         
